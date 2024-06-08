0e21c84bbe607cdcdb3bd10331a1013646d8f202
Haseeb Usman <haseebusman030576@gmail.com>
2024-06-09 02:15:48 +0500

    chore: Update about page layout

--- frontend/src/pages/about/about.jsx
+++ frontend/src/pages/about/about.jsx
@@ -218,7 +218,7 @@ const About=()=>{
               </div>
             </div>
           </section>
-          {/* <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
+          <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
             <div className="container space-y-12 px-4 md:px-6">
               <div className="flex flex-col items-center justify-center space-y-4 text-center">
                 <div className="space-y-2">
@@ -240,7 +240,7 @@ const About=()=>{
                 </div>
               </div>
             </div>
-          </section> */}
+          </section>
         </main>
       </div>
     )
